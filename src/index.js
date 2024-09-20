699dafca1926eb6442518f8591942a4ad934eee6
enidelcu <enidelcu@gmail.com>
2024-09-20 08:15:39 +0300

    enable csp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Helmet } from 'react-helmet';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,6 +8,23 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="
+          default-src 'self';
+          script-src 'self' 'unsafe-inline';
+          style-src 'self' 'unsafe-inline';
+          img-src 'self' data:;
+          connect-src 'self';
+          font-src 'self';
+          frame-src 'none';
+          object-src 'none';
+          base-uri 'self';
+          form-action 'self';
+        "
+      />
+    </Helmet>
     <App />
   </React.StrictMode>
 );
